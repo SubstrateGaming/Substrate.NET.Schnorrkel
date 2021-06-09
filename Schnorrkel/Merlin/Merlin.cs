3357c7e14f406f700b7ae9ecd950c7419e3b7009
Cedric Decoster <cedric.decoster@gmail.com>
2021-06-09 15:47:31 +0200

    fixed typo

--- Schnorrkel/Merlin/Merlin.cs
+++ Schnorrkel/Merlin/Merlin.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * Copyright (C) 2020 Usetech Professional
- * Modifications: copyright (C) 2021 DOT Mog
+ * Modifications: Copyright (C) 2021 DOT Mog
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
